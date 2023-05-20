311b75f89f7e244db9a89ca2af4d7b8ec28a6315
Mike <michaelxu17@gmail.com>
2023-05-21 00:39:48 +0700

    finished left and right building generator

--- Assets/Scripts/rightBuildingController.cs
+++ Assets/Scripts/rightBuildingController.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class leftBuildingController : MonoBehaviour
+public class rightBuildingController : MonoBehaviour
 {
     public GameObject[] leftBuilding;
     private double spawnZ = -65;
     private int TOS = 20;
-    private double x = -2.535533;
-    private double y = 2.9125;
-    private List<GameObject> active = new List<GameObject> ();
-
+    private double x = -14.9;
+    private double y = -5.182549;
 
     // Start is called before the first frame update
     void Start()
     {
-        active = new List<GameObject> ();
-         for (int i = 0; i < TOS; i++)
+        for (int i = 0; i < TOS; i++)
         {
             showBuilding();
         }
-          
+
+
     }
 
     // Update is called once per frame
@@ -31,13 +29,9 @@ public class leftBuildingController : MonoBehaviour
 
     private void showBuilding(int idx = 1)
     {
-        GameObject go;
         transform.position = Vector3.forward * (float)spawnZ;
-    
-        spawnZ = spawnZ + 61.5;
-        
-        go = Instantiate(leftBuilding[0], new Vector3((float)x, (float)y, (float)spawnZ), Quaternion.identity);
-    }
 
- 
-}   
+        spawnZ = spawnZ + 61.5;
+        Instantiate(leftBuilding[0], new Vector3((float)x, (float)y, (float)spawnZ), Quaternion.identity);
+    }
+}
